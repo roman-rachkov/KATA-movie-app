f307fadabc401a13ba1702dfbc6521fe265f4504
Rachkov Roman <pakycb84@gmail.com>
2023-06-25 16:07:03 +1000

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
   css: {
     modules: {
